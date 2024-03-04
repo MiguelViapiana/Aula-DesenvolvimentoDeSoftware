3555f9702cd7edae6844895eb21f7cee69ca26bd
Miguel Viapiana Jung <127536583+MiguelViapiana@users.noreply.github.com>
2024-03-04 11:25:54 -0300

    Exercicio 05 adicionado

---
+++ Aula03/ex005/NotaMedia.java
@@ -0,0 +1,22 @@
+package Aula03.ex005;
+
+import java.util.Scanner;
+
+public class NotaMedia {
+    public static void main(String[ ] args){
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("--- Formularia Acadêmico ---");
+        String[] aluno = new String[5];
+        int[] matriculas = new int[5];
+        boolean[] aprovado = new boolean[5];
+        double[] nota1 = new double[5];
+        double[] nota2 = new double[5];
+        double[] notaFinal = new double[5];
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("informe o nome do aluno " + (i + 1) + ":");
+
+        }
+
+    }
+}
